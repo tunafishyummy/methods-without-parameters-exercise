909f62127e20684d53dfd9c79364b7f732144df3
tunafishyummy <adinoellamio@gmail.com>
2026-03-03 15:24:25 +0800

    filename fix

--- Group_4_Base_5_module3_exercise_3_2_methodsAndWithoutParameters/Borrower.java
+++ Group_4_Base_5_module3_exercise_3_2_methodsAndWithoutParameters/Borrower.java
@@ -1,4 +1,4 @@
-package yeah;
+package Group_4_Base_5_module3_exercise_3_2_methodsAndWithoutParameters;
 public class Borrower {
     public int borrowerId;
     public String lastName;
--- Group_4_Base_5_module3_exercise_3_2_methodsAndWithoutParameters/Runner.java
+++ Group_4_Base_5_module3_exercise_3_2_methodsAndWithoutParameters/Runner.java
@@ -1,4 +1,4 @@
-package yeah;
+package Group_4_Base_5_module3_exercise_3_2_methodsAndWithoutParameters;
 
 public class Runner {
     Book book1 = new Book();
