7ae7ba8f25191e2fb2aa7e6225bdee133bb1b337
erusrxmos@gmail.com <erusrxmos@gmail.com>
2026-03-03 22:01:50 +0800

    Author class scanner removed

--- Group_4_Base_5_module3_exercise_3_2_methodsAndWithoutParameters/Author.java
+++ Group_4_Base_5_module3_exercise_3_2_methodsAndWithoutParameters/Author.java
@@ -1,22 +1,12 @@
 package Group_4_Base_5_module3_exercise_3_2_methodsAndWithoutParameters;
-import java.util.Scanner;
+
 public class Author {
     public int authorId;
     public String lastName;
     public String firstName;
     void setinfo(int authorId, String lastName, String firstName) {
-        Scanner authorGet = new Scanner(System.in);
-
-        Author author1 = new Author();
-
-        System.out.print("Enter author ID: ");
-        author1.authorId = authorGet.nextInt(); 
-        authorGet.nextLine();
-
-        System.out.print("Enter author's last name: ");
-        author1.lastName = authorGet.nextLine(); 
-
-        System.out.print("Enter author's first name: ");
-        author1.firstName = authorGet.nextLine(); 
-}
+        this.authorId = authorId;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
 }
